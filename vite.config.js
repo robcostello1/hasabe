8005638bfffea397faa9fba9c35e1c1412163abe
Rob Costello <robcostello1@gmail.com>
2023-07-31 11:05:51 +0100

    debug netflify build

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,14 @@ import svgrPlugin from "vite-plugin-svgr";
 console.log("CI =", process.env.CI);
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
-  build: {
-    chunkSizeWarningLimit: 3000,
-  },
+export default defineConfig((config) => {
+  console.log(config);
+  return {
+    ...config,
+    plugins: [...config.plugins, react(), viteTsconfigPaths(), svgrPlugin()],
+    build: {
+      ...config.build,
+      chunkSizeWarningLimit: 3000,
+    },
+  };
 });
