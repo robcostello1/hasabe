ee78d0e0b8657d8b54d8dc36cfdca4fbe18e40d7
Rob Costello <robcostello1@gmail.com>
2023-07-31 10:45:19 +0100

    testing CI value

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import react from "@vitejs/plugin-react";
 import viteTsconfigPaths from "vite-tsconfig-paths";
 import svgrPlugin from "vite-plugin-svgr";
 
+console.log("CI =", process.env.CI);
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
