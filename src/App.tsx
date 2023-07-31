38783acff11823891ffc2f1561a06075c034c807
Rob Costello <robcostello1@gmail.com>
2023-07-31 15:03:16 +0100

    added basic privacy policy

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import Hasabe from "./apps/hasabe/App";
-import Strategies from "./apps/strategies/Strategies";
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Hasabe from './apps/hasabe/App';
+import PrivacyPolicy from './apps/policies/PrivacyPolicy';
+
+// import Strategies from "./apps/strategies/Strategies";
 
 const App = () => (
   <BrowserRouter>
     <Switch>
-      <Route path="/strategies">
-        <Strategies />
+      <Route path="/privacy">
+        <PrivacyPolicy />
       </Route>
+      {/* <Route path="/strategies">
+        <Strategies />
+      </Route> */}
       <Route path="/">
         <Hasabe />
       </Route>
