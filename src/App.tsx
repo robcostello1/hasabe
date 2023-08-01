ff45b03f4c78b1b6e252d73da5faaa2b231675f9
Rob Costello <robcostello1@gmail.com>
2023-08-01 11:10:37 +0100

    added draft ts and cs

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Hasabe from './apps/hasabe/App';
 import PrivacyPolicy from './apps/policies/PrivacyPolicy';
+import TermsAndConditions from './apps/policies/TermsAndConditions';
 
 // import Strategies from "./apps/strategies/Strategies";
 
@@ -10,6 +11,9 @@ const App = () => (
     <Switch>
       <Route path="/privacy">
         <PrivacyPolicy />
+      </Route>
+      <Route path="/terms">
+        <TermsAndConditions />
       </Route>
       {/* <Route path="/strategies">
         <Strategies />
